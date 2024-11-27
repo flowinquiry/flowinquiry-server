ade956302195dc53bf38e0385a8f4eb2dbf75926
Hai Nguyen <haiphucnguyen@gmail.com>
2024-11-26 17:31:55 -0800

    Update

---
+++ server/src/main/java/io/flexwork/modules/usermanagement/service/mapper/UserMapperUtils.java
@@ -0,0 +1,16 @@
+package io.flexwork.modules.usermanagement.service.mapper;
+
+import io.flexwork.modules.usermanagement.domain.User;
+import org.mapstruct.Named;
+
+public class UserMapperUtils {
+
+    @Named("mapUserToFullName")
+    public static String mapUserToFullName(User user) {
+        return user != null
+                ? (user.getFirstName() != null ? user.getFirstName() : "")
+                        + " "
+                        + (user.getLastName() != null ? user.getLastName() : "")
+                : "";
+    }
+}

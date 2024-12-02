c996c0f015407176d57bd57060fecab74a5e4386
Hai Nguyen <haiphucnguyen@gmail.com>
2024-12-02 11:48:32 -0800

    Update

--- server/src/main/java/io/flexwork/modules/collab/service/mapper/ActivityLogMapper.java
+++ server/src/main/java/io/flexwork/modules/collab/service/mapper/ActivityLogMapper.java
@@ -4,13 +4,9 @@ import io.flexwork.modules.collab.domain.ActivityLog;
 import io.flexwork.modules.collab.service.dto.ActivityLogDTO;
 import io.flexwork.modules.usermanagement.service.mapper.UserMapperUtils;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = UserMapperUtils.class)
 public interface ActivityLogMapper {
 
-    @Mapping(source = "createdBy", target = "createdByName", qualifiedByName = "mapUserToFullName")
-    @Mapping(source = "createdBy.id", target = "createdById")
-    @Mapping(source = "createdBy.imageUrl", target = "createdByImageUrl")
     ActivityLogDTO toDTO(ActivityLog activityLog);
 }

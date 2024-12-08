2ea908ecbbcd1466a781da582d6d5b694313fe05
Hai Phuc Nguyen <haiphucnguyen@gmail.com>
2024-12-08 00:32:52 -0800

    Update

--- server/src/main/java/io/flexwork/modules/teams/service/dto/WorkflowDTO.java
+++ server/src/main/java/io/flexwork/modules/teams/service/dto/WorkflowDTO.java
@@ -11,22 +11,14 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder
 public class WorkflowDTO {
-
     private Long id;
-
     private String name;
-
     private String description;
-
     private String requestName;
-
     private Long ownerId; // ID of the owning team; null for global workflows
-
+    private String ownerName;
     private WorkflowVisibility visibility;
-
     private Integer level1EscalationTimeout;
-
     private Integer level2EscalationTimeout;
-
     private Integer level3EscalationTimeout;
 }

dbbcfa878bd355b46084ffd69b7dbb0cd74b4095
Hai Phuc Nguyen <hainguyen@flowinquiry.io>
2025-01-03 00:42:35 -0800

    Update

--- server/src/test/java/io/flowinquiry/config/CustomHibernateConnectionProvider.java
+++ server/src/test/java/io/flowinquiry/config/CustomHibernateConnectionProvider.java
@@ -3,15 +3,19 @@ package io.flowinquiry.config;
 import io.flowinquiry.db.DbConstants;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Map;
 import javax.sql.DataSource;
 import org.checkerframework.checker.initialization.qual.Initialized;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.UnknownKeyFor;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CustomHibernateConnectionProvider implements ConnectionProvider {
+public class CustomHibernateConnectionProvider
+        implements ConnectionProvider, HibernatePropertiesCustomizer {
 
     private DataSource dataSource;
 
@@ -45,5 +49,10 @@ public class CustomHibernateConnectionProvider implements ConnectionProvider {
     @Override
     public <T> T unwrap(@UnknownKeyFor @NonNull @Initialized Class<T> unwrapType) {
         return null;
+    }
+
+    @Override
+    public void customize(Map<String, Object> hibernateProperties) {
+        hibernateProperties.put(AvailableSettings.CONNECTION_PROVIDER, this);
     }
 }

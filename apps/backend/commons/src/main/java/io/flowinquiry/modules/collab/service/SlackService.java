b1465b6f3e74cb5a81cbfcd7c90d7f84dbd789ee
Omar Orphy <oorphy@aucegypt.edu>
2025-05-30 16:02:22 +0200

    feature(issue-82): Add Slack Service (WIP)

---
+++ apps/backend/commons/src/main/java/io/flowinquiry/modules/collab/service/SlackService.java
@@ -0,0 +1,53 @@
+package io.flowinquiry.modules.collab.service;
+
+import com.slack.api.Slack;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+import io.flowinquiry.config.FlowInquiryProperties;
+import io.flowinquiry.modules.collab.domain.SlackMessage;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class SlackService {
+
+    private final FlowInquiryProperties flowInquiryProperties;
+
+    private final Slack slack = Slack.getInstance();
+
+    public SlackService(FlowInquiryProperties flowInquiryProperties) {
+        this.flowInquiryProperties = flowInquiryProperties;
+    }
+
+    public ChatPostMessageResponse sendSlackMessage(SlackMessage slackMessage)
+            throws IOException, SlackApiException {
+        String slackToken = flowInquiryProperties.getSlack().getToken();
+        String message = slackMessage.getMessage();
+        String channelId = slackMessage.getChannelId();
+
+        // Build a request object
+        ChatPostMessageRequest request =
+                ChatPostMessageRequest.builder()
+                        .channel(channelId) // Use a channel ID `C1234567` is preferable
+                        .text(message)
+                        .build();
+
+        ChatPostMessageResponse messageResponse =
+                slack.methods(slackToken).chatPostMessage(request);
+        if (messageResponse.isOk()) {
+            log.debug(
+                    "Slack message successfully sent. Message: {} channel: {}", message, channelId);
+        } else {
+            log.error(
+                    "Sending Slack message {} to channel {} failed due to {}",
+                    message,
+                    channelId,
+                    messageResponse.getError());
+        }
+
+        return messageResponse;
+    }
+}
